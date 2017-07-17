322fbad7860a90e986ba4745002520c62e3143f8
slcwestdevcamp <slcwest@devcamp.com>
2017-07-17 16:49:29 +0000

    Implemented persistent position updates for drag and drop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,34 +6,38 @@ class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.by_position
   end
-  
+
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+
+  render nothing: true
+  end
   def angular
     @angular_portfolio_items = Portfolio.angular
   end
-  
+
   def new
     @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build}
-  end  
-  
-  def show
+    3.times { @portfolio_item.technologies.build }
   end
-  
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'your post is now live.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
@@ -43,30 +47,31 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
-  
+
+  def show
+  end
 
   def destroy
-    #Destroy/delete the record
+    # Destroy/delete the record
     @portfolio_item.destroy
-    
+
     # Redirect
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Record was removed.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
-  end 
-  
+  end
+
   private
-  
+
   def portfolio_params
-    params.require(:portfolio).permit(:title, 
-                                      :subtitle, 
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
                                       :body,
                                       technologies_attributes: [:name]
                                      )
-  end  
-  
+  end
+
   def set_portfolio_item
-     @portfolio_item = Portfolio.find(params[:id])
-   end
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-  resources :portfolios, except: [:show]
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  resources :portfolios, except: [:show] do
+    put :sort, on: :collection
+  end
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
@@ -8,7 +11,7 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
 
   resources :blogs do
-    member do 
+    member do
       get :toggle_status
     end
   end
