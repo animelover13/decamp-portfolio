f2803413182dc44b2f41160cbd12a8fe99b7a885
slcwestdevcamp <slcwest@devcamp.com>
2017-07-31 16:20:27 +0000

    Implemented twitter call on tech new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
-
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
@@ -9,6 +8,7 @@ Rails.application.routes.draw do
 
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
 
   resources :blogs do
     member do
