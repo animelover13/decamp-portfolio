bbfee98e592aed197090844ecb3331f24c8cf4c6
slcwestdevcamp <slcwest@devcamp.com>
2017-08-05 09:27:22 +0000

    Added view component for skills

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,16 @@
 class PagesController < ApplicationController
   def home
     @posts = Blog.all
+    @skills = Skill.all
   end
 
   def about
+    @skills = Skill.all
   end
 
   def contact
   end
-  
+
   def tech_news
     @tweets = SocialTool.twitter_search
   end
