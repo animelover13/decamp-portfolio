23d963016adc2f9207be45e68e46538caa47423c
slcwestdevcamp <slcwest@devcamp.com>
2017-08-05 18:03:01 +0000

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
+User.create!(
+  email: "Test@test.com",
+  password: "Shadow20",
+  password_confirmation: "Shadow20",
+  name: "Admin User", 
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "Test2@test.com",
+  password: "Shadow20",
+  password_confirmation: "Shadow20",
+  name: "Regular User", 
+)
+
+puts "1 regular user created"
+
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
